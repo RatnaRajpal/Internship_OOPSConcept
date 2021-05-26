072234f794f663dc0123708e257d292552a814f0
RatnaRajpal <54902237+RatnaRajpal@users.noreply.github.com>
2021-05-26 19:08:15 +0530

    Create Square.java

---
+++ Quadrilateral_Inheritance/Square.java
@@ -0,0 +1,16 @@
+/*Write an inheritance hierarchy for classes Quadrilateral, Trapezoid, Parallelogram, Rectangle and Square.
+Use Quadrilateral as the super class of the hierarchy. The instance variables of Quadrilateral should be
+the x-y coordinate pairs for the four endpoints of the Quadrilateral. Write a program that instantiates
+objects of your classes and outputs each object's area (except Quadrilateral)*/
+package internship_Foxmula;
+
+public class Square extends Quadrilateral{
+  
+Square(int xy[][]){
+	setcoordinate(xy);
+}
+int area(){
+    int ar=(int)Math.sqrt((xy[0][0]-xy[1][0])*(xy[0][0]-xy[1][0])+(xy[0][1]-xy[1][1])*(xy[0][1]-xy[1][1]));
+    return ar*ar;
+}
+}
