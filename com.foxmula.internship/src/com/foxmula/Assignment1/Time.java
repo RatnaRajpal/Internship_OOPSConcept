aaeae9f44c27504b718bef9e0bd828959c2d1de6
RatnaRajpal <54902237+RatnaRajpal@users.noreply.github.com>
2021-05-29 15:41:25 +0530

    Add files via upload

---
+++ com.foxmula.internship/src/com/foxmula/Assignment1/Time.java
@@ -0,0 +1,63 @@
+package com.foxmula.Assignment1;
+import java.util.*; 
+
+public class Time {
+
+	private int hr,min,sec;
+	Time(int hr,int min,int sec){
+		this.hr=hr;
+		this.min=min;
+		this.sec=sec;
+	}
+    
+	Time(){
+		hr=0;
+		min=0;
+		sec=0;
+	}
+    //Function to add the two times
+	public Time add(Time time){
+		Time t=new Time();
+		t.sec=this.sec+time.sec;
+		if(t.sec>=60)
+			{
+		
+			t.min=(int)t.sec/60;
+			t.sec=t.sec%60;
+		}
+		
+		t.min+=this.min+time.min;
+		if(t.min>=60){
+			
+			t.hr=(int)t.min/60;
+			t.min=t.min%60;
+		}
+		
+		t.hr+=this.hr+time.hr;
+		if(t.hr>24)
+			t.hr-=24;
+		
+		return t;
+		
+	}
+	void display(String str) {
+		System.out.println(str+""+hr+":"+min+":"+sec);
+	}
+	
+	public static void main(String []args){
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter the time in (hour:minute:second) format");
+		Time time1=new Time(sc.nextInt(),sc.nextInt(),sc.nextInt());
+		
+		System.out.println("Enter the time to add in (hour:minute:second) format");
+		Time time2=new Time(sc.nextInt(),sc.nextInt(),sc.nextInt());
+		
+		Time time3=time1.add(time2);
+		
+		time1.display("Time A:  ");
+		time2.display("Time B:  ");
+		time3.display("Time after addition:  ");
+		}
+}
+
